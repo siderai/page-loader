0d795a7d8efb00d28a7d25dbb6deb5e5d4736437
Alexander Sidorov <pl4zie@gmail.com>
2022-02-11 00:36:10 +0300

    Fix relative paths in final

--- page_loader/core.py
+++ page_loader/core.py
@@ -42,23 +42,26 @@ def download(url: str, content_path: str) -> str:
     # save image from page, then replace its url by path of downloaded file
     img_items = soup.find_all('img')
     for img in img_items:
-        local_path_to_img = save_image(img, content_dir_name, url)
+        local_path_to_img = save_image(img, path_for_files, url)
         if local_path_to_img:
-            img['src'] = local_path_to_img
+            relpath = os.path.relpath(local_path_to_img, content_dir_name)
+            img['src'] = relpath
 
     # save JS files
     scripts = soup.find_all('script')
     for script in scripts:
-        local_path_to_script = save_script(script, content_dir_name, url)
+        local_path_to_script = save_script(script, path_for_files, url)
         if local_path_to_script:
-            script['src'] = local_path_to_script
+            relpath = os.path.relpath(local_path_to_script, content_dir_name)
+            script['src'] = relpath
 
     # save CSS
     resources = soup.find_all('link')
     for res in resources:
-        local_path_to_res = save_resource(res, content_dir_name, url)
+        local_path_to_res = save_resource(res, path_for_files, url)
         if local_path_to_res:
-            res['href'] = local_path_to_res
+            relpath = os.path.relpath(local_path_to_res, content_dir_name)
+            res['href'] = relpath
 
     # save page as html locally
 
